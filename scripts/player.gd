9ef7ebe6de3867e84f66d85d0ee758dfd0de3d56
AirbusA340 <elims@wsc.school.nz>
2024-08-22 12:30:56 +1200

    controls part 4

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,48 +1,42 @@
 extends CharacterBody2D
 
-
+var speed: float = 300
+var accel: float = 100
+var y_speed: float = 5000
+var y_accel: float = 5000
 const SPEED = 300.0
-const JUMP_VELOCITY = -200.0
-var character_direction: Vector2
-var movement_speed: float = 500
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	# Handle jump.
+func _physics_process(_delta):
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "flip_up", "flip_down")
+
+	var direction_x = Input.get_axis("move_left", "move_right")
+
+	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
+	velocity.y = move_toward(velocity.y, y_speed * direction.y, y_accel)
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("flip") and is_on_floor():
+	if Input.is_action_just_pressed("flip_up"):
 		animated_sprite.flip_v = true
-		velocity = character_direction * movement_speed
-	elif Input.is_action_just_pressed("flip") and not is_on_floor_only:
+
+	elif Input.is_action_just_pressed("flip_down"):
 		animated_sprite.flip_v = false
-		velocity = character_direction * movement_speed
+
 	
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
+	if direction_x > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif direction_x < 0:
 		animated_sprite.flip_h = true
 	
-	if direction == 0:
+	if direction_x == 0:
 		animated_sprite.play("Idle")
 	else:
 		animated_sprite.play("Run")
 		
-	if direction:
-		velocity.x = direction * SPEED
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED)	
+	
 	move_and_slide()
