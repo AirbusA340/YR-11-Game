d4f730dc0805e3b762f0af94da2fc60559c7825e
AirbusA340 <elims@wsc.school.nz>
2024-08-23 13:02:03 +1200

    controls part 5

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,21 @@ var accel: float = 100
 var y_speed: float = 5000
 var y_accel: float = 5000
 const SPEED = 300.0
+const JUMP_VELOCITY = -200.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var collision_shape = $CollisionShape2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _physics_process(_delta):
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		if not is_on_floor():
+			velocity.y += gravity * _delta
+			
+	
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "flip_up", "flip_down")
 
 	var direction_x = Input.get_axis("move_left", "move_right")
@@ -19,7 +29,6 @@ func _physics_process(_delta):
 	
 	if Input.is_action_just_pressed("flip_up"):
 		animated_sprite.flip_v = true
-
 	elif Input.is_action_just_pressed("flip_down"):
 		animated_sprite.flip_v = false
 
