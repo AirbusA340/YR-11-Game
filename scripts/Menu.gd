ee1716b09894c89c0cc0e31243dfd9aa7d2d6133
AirbusA340 <elims@wsc.school.nz>
2024-08-30 13:03:09 +1200

    UI Part 3

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,12 +8,12 @@ func _on_play_pressed():
 
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
 
 func _on_customize_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/customize.tscn")
 
 
 func _on_quit_pressed():
--- scripts/level_selection.gd
+++ scripts/level_selection.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_go_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
