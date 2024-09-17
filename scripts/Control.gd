15db109133d8bc1beaba830595d7f510aa664b04
AirbusA340 <elims@wsc.school.nz>
2024-09-17 12:30:49 +1200

    finished the menus

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -1,14 +1,41 @@
 extends Control
 
-
+func _ready():
+	$AnimationPlayer.play("RESET")
 # Called when the node enters the scene tree for the first time.
+
 func resume():
 	get_tree().paused = false
-
+	$AnimationPlayer.play_backwards("blur")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func pause(delta):
+func pause():
 	get_tree().paused = true
+	$AnimationPlayer.play("blur")
 	
 func testEsc():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
 	
+func _on_resume_pressed():
+	resume()
+	
+
+
+
+func _on_restart_pressed():
+	resume()
+	get_tree().reload_current_scene()
+
+
+
+func _on_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/level_selection.tscn")
+	
+	
+func _process(_delta):
+	testEsc()
